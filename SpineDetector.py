7623e77fa6b8b170b79fb288072f692ed45f0f19
mikeusru <Mishkin86>
2019-04-25 12:18:34 -0400

    working on a more efficient sliding window method

--- SpineDetector.py
+++ SpineDetector.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 from keras.models import model_from_json
 import keras.backend as K
+from skimage import transform
 
 from model_data.model import yolo_eval
 from model_data.utils import letterbox_image
@@ -17,6 +18,7 @@ class SpineDetector:
         self.score_threshold = 0.3
         self.iou_threshold = 0.45
         self.model_image_size = (416, 416)
+        self.target_scale_pixels_per_um = 10
         self.anchors = self._load_anchors()
         self.model = self._load_model()
         self.boxes, self.scores, self.classes = self._generate_output_tensors()
@@ -44,6 +46,13 @@ class SpineDetector:
                                            iou_threshold=self.iou_threshold)
         return boxes, scores, classes
 
+    def _rescale_image(self, image, original_scale):
+        resize_ratio = self.target_scale_pixels_per_um / original_scale
+        new_shape = np.array(image.shape)
+        new_shape[:2] = np.array(new_shape[:2] * resize_ratio, dtype=np.int)
+        image_rescaled = transform.resize(image, new_shape, preserve_range=True).astype(np.uint8)
+        return image_rescaled, resize_ratio
+
     def detect_spines_in_image_list(self, image_list):
         boxes = []
         scores = []
@@ -66,3 +75,19 @@ class SpineDetector:
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
         return image_data
+
+    def _split_image(self, image):
+        window_size = self.model_image_size
+        window_step = int(self.model_image_size[0] / 2)
+
+    def yield_sliding_windows(self, image, sliding_window_step, sliding_window_side):
+        for y in range(0, image.shape[0], sliding_window_step):
+            for x in range(0, image.shape[1], sliding_window_step):
+                img_window = image[y:y + self.sliding_window_side, x:x + self.sliding_window_side]
+                yield (x, y, img_window, boxes_in_window)
+
+    def run(self, image, scale):
+        image, resize_ratio = self._rescale_image(image, scale)
+        image_list = self._split_image(image)
+
+#TODO: Instead of creating separate windows, just create an array of indices for pulling out images from the resized image
