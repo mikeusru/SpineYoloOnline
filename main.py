6bc1e625ce93d374dd7b8420915b1ec845d2f967
mikeusru <Mishkin86>
2019-04-26 14:43:30 -0400

    added .yml file

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 import time
 from SpineDetector import SpineDetector
 
+
+strBucket = 'spine-yolo-serverless'
+
 def save_results(image, boxes):
     sub_path = 'results'
     if not os.path.isdir(sub_path):
@@ -10,6 +13,7 @@ def save_results(image, boxes):
     img_path_relative = os.path.join(sub_path, 'r_img' + timestr + '.jpg')
     image.save(img_path_relative)
 
+
 detector = SpineDetector()
 r_image, boxes_scores = detector.find_spines('temp/000002.tif', 10)
-save_results(r_image, boxes_scores)
+# save_results(r_image, boxes_scores)
