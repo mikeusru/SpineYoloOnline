901e723a5f4cf1a5e4818519326e6ec3aefbe16d
mikeusru <Mishkin86>
2019-07-25 09:34:37 -0400

    added project ID to build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 docker build -t predict-service .
-docker tag predict-service gcr.io/PROJECT_ID/predict-service
-gcloud docker -- push gcr.io/PROJECT_ID/predict-service
+docker tag predict-service gcr.io/spine-yolo/predict-service
+gcloud docker -- push gcr.io/spine-yolo/predict-service
